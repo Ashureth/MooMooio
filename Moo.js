0385a6e8253ca947d18646a191bd2a9c718ce798
Ashureth <126100460+Ashureth@users.noreply.github.com>
2023-03-18 16:41:42 +0100

    Update Moo.js

--- Moo.js
+++ Moo.js
@@ -6,11 +6,7 @@ class MooMoo {
       WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
         apply(target, socket, data) {
           WebSocket.prototype.send = target;
-          
-          if(!this.socket.length || this.socket.url != socket.url || this.socket.readyState > 1) {
-              this.socket = socket;
-              console.log('Hooked', this.socket)
-          }
+          console.log(socket, this.socket)
           
           Reflect.apply(target, socket, data);
         }
