1034ba0713e18492d918381da25dc2fe0a523aa8
Ashureth <126100460+Ashureth@users.noreply.github.com>
2023-03-18 16:43:04 +0100

    Update Moo.js

--- Moo.js
+++ Moo.js
@@ -1,12 +1,11 @@
 class MooMoo {
   constructor(started) {
-    this.socket = {};
     this.started = started;
     this.fetchSocket = function() {
       WebSocket.prototype.send = new Proxy(WebSocket.prototype.send, {
         apply(target, socket, data) {
-          WebSocket.prototype.send = target;
-          console.log(socket, this.socket)
+          
+          console.log(socket)
           
           Reflect.apply(target, socket, data);
         }
